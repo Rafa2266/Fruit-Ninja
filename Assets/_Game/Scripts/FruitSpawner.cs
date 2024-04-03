9fe1ad03e17a5653dcb05fe9bbd364a9c5dfaf6e
Rafael Franca Scofield Lauar <rafavasco12@hotmail.com>
2024-04-02 23:40:43 -0300

    criando spawn dos objetos

---
+++ Assets/_Game/Scripts/FruitSpawner.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FruitSpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject[] fruitsPrefab;
+    [SerializeField] private Transform[] spawnPoints;
+    [SerializeField] private float minDelay, maxDelay;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Spawn());
+    }
+    private IEnumerator Spawn()
+    {
+        while (true)
+        {
+            float delay = Random.Range(minDelay, maxDelay);
+            yield return new WaitForSeconds(delay);
+            
+            int spawnIndex=Random.Range(0, spawnPoints.Length);
+            Transform spawnPoint= spawnPoints[spawnIndex];
+
+            GameObject fruitPrefab = Instantiate(fruitsPrefab[Random.Range(0, fruitsPrefab.Length)], spawnPoint.position, spawnPoint.rotation);
+            Destroy(fruitPrefab,5f);
+        }
+    }
+
+}
