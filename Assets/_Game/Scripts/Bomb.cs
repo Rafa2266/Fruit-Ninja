1126ea1bb689344a943628221132967bc2dc17a1
Rafael Franca Scofield Lauar <rafavasco12@hotmail.com>
2024-04-22 22:51:29 -0300

    programação da bomba adicionada

--- Assets/_Game/Scripts/Bomb.cs
+++ Assets/_Game/Scripts/Bomb.cs
@@ -5,10 +5,15 @@ using UnityEngine;
 public class Bomb : MonoBehaviour
 {
     // Start is called before the first frame update
-    [SerializeField] private float speed;
+    [SerializeField] private float speed,startForce;
+    [SerializeField] private GameObject lightBeam;
+    private Rigidbody2D myRb;
+    private float sentidoRotation;
     void Start()
     {
-        
+        myRb = this.gameObject.GetComponent<Rigidbody2D>();
+        ApplyForce();
+        sentidoRotation = Random.Range(-1f, 1f);
     }
 
     // Update is called once per frame
@@ -18,6 +23,23 @@ public class Bomb : MonoBehaviour
     }
     private void Rotate()
     {
-        transform.Rotate(new Vector3(0f,speed,0f)*Time.deltaTime);
+        float speedRotation = Random.Range(speed,speed*2);
+        speedRotation *= sentidoRotation;
+        transform.Rotate(new Vector3(speedRotation,speedRotation,0f)*Time.deltaTime);
+    }
+    private void ApplyForce()
+    {
+
+        myRb.AddForce(transform.up * Random.Range(startForce - 6, startForce), ForceMode2D.Impulse);
+
+    }
+    public void BombGameOver()
+    {
+        speed= 0f;
+        myRb.bodyType = RigidbodyType2D.Kinematic;
+        myRb.simulated = false;
+        CircleCollider2D myCollider=this.gameObject.GetComponent<CircleCollider2D>();
+        myCollider.enabled= false;
+        GameObject tempLightBeam= Instantiate(lightBeam,this.transform.position,Quaternion.identity) as GameObject;
     }
 }
